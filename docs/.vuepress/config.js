f231934314096794ac0fe11de1081fb595f4ca35
toesbieya <1647775459@qq.com>
2020-07-20 22:13:44 +0800

    文档更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,7 +54,8 @@ module.exports = {
                         ]
                     },
                     '/frontend/utils',
-                    '/frontend/plugins'
+                    '/frontend/plugins',
+                    '/frontend/mock'
                 ]
             },
             {
