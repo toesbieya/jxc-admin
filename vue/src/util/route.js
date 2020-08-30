12a025095fdffdb02c3ed251d0f51c0d8d392c65
toesbieya <1647775459@qq.com>
2020-08-30 12:13:40 +0800

    修复@/util/route.js中的跳转错误

--- vue/src/util/route.js
+++ vue/src/util/route.js
@@ -3,6 +3,6 @@ import router from '@/router'
 import {mutations as tagsViewMutations} from "@/layout/store/tagsView"
 
 export function closeCurrentPage(next) {
-    tagsViewMutations.delView(router.currentPage)
+    tagsViewMutations.delView(router.currentRoute)
     !isEmpty(next) && router.replace(next)
 }
