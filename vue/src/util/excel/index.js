e643001820cbda135e73cf1ea20c70b8f66a91ec
toesbieya <1647775459@qq.com>
2020-09-18 15:34:59 +0800

    \u0020

--- vue/src/util/excel/index.js
+++ vue/src/util/excel/index.js
@@ -1,11 +1,1 @@
-import {exportExcel, json2workbook, workbook2excel} from "./exceljs"
-
-/**
- * @desc el-table导出excel
- * @param tableInstance el-table实例
- */
-export function elementUiTable2excel(tableInstance) {
-
-}
-
-export {exportExcel, json2workbook, workbook2excel}
+export {exportExcel, json2workbook, workbook2excel} from "./exceljs"
